51636cfc7d975e3fe5a1141fd669e87b042dc5e2
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-12-30 01:35:14 +0530

    add service

---
+++ .github/workflows/index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+fs.readFile("./cypress/results/mochawesome.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
