4ba255df63c1734c15886a2406c93538150f6c58
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-12-30 12:34:50 +0530

    test

--- .github/workflows/cypress/e2e/test.cy.js
+++ .github/workflows/cypress/e2e/test.cy.js
@@ -9,4 +9,15 @@ describe("Basic Server Tests", () => {
       expect(response.body).to.equal("Welcome to the world!");
     });
   });
+
+  it("should return the correct welcome message", () => {
+    // Visit the root URL
+    cy.request("GET", "/").then((response) => {
+      // Check the status code
+      expect(response.status).to.equal(200);
+
+      // Check the response body
+      expect(response.body).to.equal("Welcome to the world! .");
+    });
+  });
 });
