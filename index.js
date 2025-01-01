fc182fd3e09446e819f24755cd6ec23a64c6e1c8
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2025-01-01 10:52:57 +0530

    test

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 require("dotenv").config();
-const express = require("express");
 const PORT = process.env.PORT || 8090;
-
-
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Welcome to the world!");
-});
-
-app.listen(PORT, () => {
-  console.log("listening on port " + PORT + "...");
-});
